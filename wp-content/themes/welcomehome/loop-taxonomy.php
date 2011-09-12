96af6d88d126a5b44564ee4385946113991d18f9
Sean Herron <seanherron@gmail.com>
2011-09-11 21:24:02 -0400

    Adding taxonomy loop

---
+++ wp-content/themes/welcomehome/loop-taxonomy.php
@@ -0,0 +1,25 @@
+<?php /* If there are no posts to display, such as an empty archive page */ ?>
+<?php if (!have_posts()) : ?>
+	<div class="notice">
+		<p class="bottom"><?php _e('Sorry, no results were found.', 'roots'); ?></p>
+	</div>
+	<?php get_search_form(); ?>	
+<?php endif; ?>
+
+<?php /* Start loop */ ?>
+<?php while (have_posts()) : the_post(); ?>	
+	<article id="post-<?php the_ID(); ?>" class="post">
+		<header>
+			<date><?php the_time('F jS, Y') ?></date>
+			<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a></header>
+		<?php the_excerpt(); ?>
+	</article>			
+<?php endwhile; // End the loop ?>
+
+<?php /* Display navigation to next/previous pages when applicable */ ?>
+<?php if ($wp_query->max_num_pages > 1) : ?>
+	<nav id="post-nav">
+		<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'roots' ) ); ?></div>
+		<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'roots' ) ); ?></div>
+	</nav>
+<?php endif; ?>
