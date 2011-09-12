94330190eb75ecfb3d02d8be3d23ac62dacf35a0
Sean Herron <seanherron@gmail.com>
2011-09-12 15:17:09 -0400

    Adding in single post pages

--- wp-content/themes/welcomehome/single.php
+++ wp-content/themes/welcomehome/single.php
@@ -1,24 +1,14 @@
 <?php get_header(); ?>
 	<?php roots_content_before(); ?>
-		<div id="content" class="<?php echo $roots_options['container_class']; ?>">	
-		<?php roots_main_before(); ?>
-			<div id="main" class="<?php echo $roots_options['main_class']; ?>" role="main">
-				<div class="container">
-					<?php roots_loop_before(); ?>
-					<?php get_template_part('loop', 'single'); ?>
-					<?php roots_loop_after(); ?>
-				</div>	
-			</div><!-- /#main -->
-		<?php roots_main_after(); ?>
-		<?php roots_sidebar_before(); ?>			
-			<aside id="sidebar" class="<?php echo $roots_options['sidebar_class']; ?>" role="complementary">
-			<?php roots_sidebar_inside_before(); ?>
-				<div class="container">
-					<?php get_sidebar(); ?>
-				</div>
-			<?php roots_sidebar_inside_after(); ?>
-			</aside><!-- /#sidebar -->		
-		<?php roots_sidebar_after(); ?>
-		</div><!-- /#content -->
+		<header class="expedition clearfix">
+			<h2><?php the_title(); ?></h2>
+			<img class="patch" src="<?php echo do_shortcode('[xy_expedition field="patch"]'); ?>" alt="Expedition 28 Patch" align="right" style="margin-top:-10px;"/>
+			<?php echo get_the_term_list($post->ID,  'expedition'); ?>
+			<date><?php the_time('F jS, Y') ?></date>
+			</header>
+		<?php roots_loop_before(); ?>
+		<section id="content" class="clearfix">
+		<?php get_template_part('loop', 'single'); ?>
+		</section>
 	<?php roots_content_after(); ?>
 <?php get_footer(); ?>
